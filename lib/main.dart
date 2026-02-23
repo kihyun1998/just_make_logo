38ef2b9f5cc04da35a86cdbcc8c881afd76af22e
Ki Hyun Park <kihyun1998@penta.co.kr>
2026-02-23 10:58:33 +0900

    Extract PixelArtPainter into separate file for cleaner separation of concerns

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pixel_font/pixel_font.dart';
+import 'pixel_art_painter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -79,60 +79,5 @@ class _PixelArtPageState extends State<PixelArtPage> {
         ),
       ),
     );
-  }
-}
-
-class PixelArtPainter extends CustomPainter {
-  final String text;
-
-  PixelArtPainter(this.text);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    if (text.isEmpty) return;
-
-    final chars = text.split('');
-    final totalWidth = chars.length * charWidth + (chars.length - 1) * charGap;
-
-    final pixelW = size.width / totalWidth;
-    final pixelH = size.height / charHeight;
-    final pixelSize = pixelW < pixelH ? pixelW : pixelH;
-
-    final gridPixelWidth = totalWidth * pixelSize;
-    final gridPixelHeight = charHeight * pixelSize;
-    final offsetX = (size.width - gridPixelWidth) / 2;
-    final offsetY = (size.height - gridPixelHeight) / 2;
-
-    final paint = Paint()
-      ..color = Colors.black
-      ..style = PaintingStyle.fill;
-
-    var cursorX = 0;
-    for (final char in chars) {
-      final glyph = pixelFont[char];
-      if (glyph != null) {
-        for (var row = 0; row < charHeight; row++) {
-          for (var col = 0; col < charWidth; col++) {
-            if (glyph[row][col] == 1) {
-              canvas.drawRect(
-                Rect.fromLTWH(
-                  offsetX + (cursorX + col) * pixelSize,
-                  offsetY + row * pixelSize,
-                  pixelSize,
-                  pixelSize,
-                ),
-                paint,
-              );
-            }
-          }
-        }
-      }
-      cursorX += charWidth + charGap;
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant PixelArtPainter oldDelegate) {
-    return oldDelegate.text != text;
   }
 }
