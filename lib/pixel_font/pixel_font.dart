09c4b78ad98ac2ea3fad6a1e21f320e386a1f31d
Ki Hyun Park <kihyun1998@penta.co.kr>
2026-02-23 14:23:48 +0900

    make a

--- lib/pixel_font/pixel_font.dart
+++ lib/pixel_font/pixel_font.dart
@@ -2,8 +2,8 @@ import 'alphabets.dart';
 import 'numbers.dart';
 import 'symbols.dart';
 
-const int charWidth = 14;
-const int charHeight = 18;
+const int charWidth = 16;
+const int charHeight = 16;
 const int charGap = 2;
 
 final Map<String, List<List<int>>> pixelFont = {
